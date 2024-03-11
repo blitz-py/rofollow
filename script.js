5a807ceaa036bade1d47e48e5b407fe5a5350cc7
blitz † <157614069+blitz-py@users.noreply.github.com>
2024-03-11 15:12:05 +0000

    base

---
+++ script.js
@@ -0,0 +1,29 @@
+function verifyAccount() {
+  // where the verification script belongs
+  // check if the random bio words r there
+  // display post-verification features if successful
+  document.getElementById("postVerification").style.display = "block";
+}
+
+function logout() {
+  // logout logic goes here
+  // just signs out and deletes saved cookies
+}
+
+function openDonatePage() {
+  // opens donation page feel free to change
+  window.open("https://buymeacoffee.com/blitz-py");
+}
+
+function startFollowing() {
+  // script to auto follow other rofollow users
+}
+
+function connectVPN() {
+  // script to connect to vpn
+  // changes colours
+  document.body.style.backgroundColor = "#000";
+  document.getElementById("vpnOption").style.display = "none"; //hides the vpn button
+}
+
+// add more stuff for updates etc
